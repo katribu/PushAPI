c74ce544f0b452a9c75574c84c50cd191df83b40
katb <katrinaburwash_17@hotmail.com>
2023-02-08 13:33:39 +0100

    added create and insert into queries to sql

--- pushScratchPad.sql
+++ pushScratchPad.sql
@@ -11,9 +11,20 @@ INSERT INTO users (name, email, password)
     ('Irgen Sorensen', 'irgen_w.s@hotmail.com', '007'),
 	('Shahin Hemat','shahinhemat@gmail.com', '98765');
 
--- Temporary table........
--- CREATE TABLE push-notifications (
---   ID SERIAL PRIMARY KEY,
---   remindertype TEXT,
---   geolocationtype TEXT,
--- );
+
+create table users_notification_monitor (
+id SERIAL PRIMARY KEY,
+type TEXT,
+data JSONB,
+user_id INTEGER,
+FOREIGN KEY (user_id) REFERENCES users (id)
+)
+
+INSERT INTO users_notification_monitor (type,data,user_id)
+VALUES ('location', 
+		'{"time":"14:00",
+		"message":"Pick up spare tire from Clas Ohlsen",
+		"lat":"59.913952748612104",
+		"lng":"10.74709874232724"
+		}'::jsonb,
+	   2)
